fca894acfb02c385ee3c3329745ef5e613eed080
Andrew Dupont <github@andrewdupont.net>
2026-05-09 21:40:27 -0700

    Tweak `Logger` and use it in `Pty`

--- src/log.ts
+++ src/log.ts
@@ -1,3 +1,5 @@
+// A module that logs to the console if the `advanced.enableDebugLogging`
+// setting is enabled.
 import { Disposable } from 'atom';
 
 const TAG = `[terminal] `;
@@ -5,7 +7,10 @@ const TAG = `[terminal] `;
 let enabled = false;
 let subscription: Disposable | undefined = undefined;
 
+// Must be called at least once before logging can begin. If you're unsure,
+// just call it; it's idempotent!
 export function initialize () {
+  if (subscription) return;
   subscription = atom.config.observe('terminal.advanced.enableDebugLogging', (newValue) => {
     enabled = newValue;
   });
@@ -13,6 +18,7 @@ export function initialize () {
 
 export function destroy () {
   subscription?.dispose();
+  subscription = undefined;
 }
 
 export function log (...args: unknown[]) {
